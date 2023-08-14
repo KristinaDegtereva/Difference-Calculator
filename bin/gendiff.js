792573fd59e44799cc614674a4662f4294f634af
Kristalluna@yandex.ru <a2@MacBook-Pro--Kris.local>
2023-08-14 21:03:40 +0300

    update code

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
-import { program } from 'commander';
+import { Command } from 'commander';
+const program = new Command();
 
 program
-  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.8.0')
-  .option(' -V, --version', 'output the version number')
-  .option('-h, --help', ' output usage information')
+  .argument(' <filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
