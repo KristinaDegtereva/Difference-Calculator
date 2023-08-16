be225948972ccc546e73ab8daa8b35ff92e2556d
Kristalluna@yandex.ru <a2@MacBook-Pro--Kris.local>
2023-08-17 01:08:37 +0300

    add action at gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import genfiff from '../src/index.js';
+
 const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.8.0')
-  .argument(' <filepath1>')
+  .argument('<filepath1>')
   .argument('<filepath2>')
-  .option('-f, --format <type>', 'output format');
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    const diff = genfiff(filepath1, filepath2);
+    console.log(diff);
+  });
 
 program.parse();
