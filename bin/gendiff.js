be9b54b6bdd6e01734657d4fe3f917b28dfe9093
Kristalluna@yandex.ru <a2@MacBook-Pro--Kris.local>
2023-08-26 17:20:44 +0300

    add plain format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 import gendiff from '../src/index.js';
-import stylish from '../src/stylish.js';
 
 const program = new Command();
 
@@ -10,9 +9,9 @@ program
   .version('0.8.0')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .option('-f, --format <type>', 'output format', stylish, stylish)
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    const diff = gendiff(filepath1, filepath2, program.format);
+    const diff = gendiff(filepath1, filepath2, program.opts().format);
     console.log(diff);
   });
 
